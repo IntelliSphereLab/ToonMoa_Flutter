491057fc749d3602989c943f1c5d83689fc9ed4f
qndrl1004 <qndrl1004@gmail.com>
2024-01-08 22:52:16 +0900

    toonmoa frame work4

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: collection_methods_unrelated_type
 
 import 'dart:convert';
-import 'dart:developer';
+
 import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_detail_model.dart';
 import 'package:toonflix/models/webtoon_episode_model.dart';
@@ -51,27 +51,6 @@ class ApiService {
       return episodesInstances;
     }
     throw Error();
-  }
-
-  static Future<bool> loginWithKakao() async {
-    try {
-      var uri = Uri.parse('http://localhost:4000/member/kakao/callback');
-      var response = await http.get(uri);
-      if (response.statusCode == 200) {
-        // 응답이 성공적으로 받아왔을 때 데이터 처리
-
-        // 이제 responseData를 사용하여 데이터 처리
-        return true;
-      } else {
-        // 서버에서 오류 응답을 받았을 때 처리
-        print("HTTP 오류: ${response.statusCode}");
-        return false;
-      }
-    } catch (e) {
-      // 예외 처리
-      print("오류가 발생했습니다: $e");
-      return false;
-    }
   }
 
   static Future<bool> loginWithGoogle(String googleToken) async {
