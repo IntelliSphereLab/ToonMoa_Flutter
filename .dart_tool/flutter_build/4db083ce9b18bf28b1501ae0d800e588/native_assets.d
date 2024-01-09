8ce1a5e36f8e691cdd8ed4f4c3e7ed46894b928e
한창윤 <hansyooni11@gmail.com>
2024-01-09 19:28:42 +0900

    kakao login

---
+++ .dart_tool/flutter_build/4db083ce9b18bf28b1501ae0d800e588/native_assets.d
@@ -0,0 +1,1 @@
+ /Users/codingmbair/Desktop/ToonMoa_Flutter/.dart_tool/flutter_build/4db083ce9b18bf28b1501ae0d800e588/native_assets.yaml: 
--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
-import 'package:toonflix/screens/login_screen.dart';
+import 'package:toonquirrel/screens/login_screen.dart';
 
 class FirstScreen extends StatefulWidget {
   const FirstScreen({super.key});
