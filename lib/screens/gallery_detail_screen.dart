5eb89c8a7aa0fa2e245546993a5dd87ab34cb6b4
HanChangYun1 <hansyooni11@gmail.com>
2024-01-13 22:09:07 +0900

    build gallery detail
    build gallery detail

--- lib/screens/gallery_detail_screen.dart
+++ lib/screens/gallery_detail_screen.dart
@@ -1,9 +1,11 @@
-// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: library_private_types_in_public_api, avoid_print
 
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toonquirrel/models/gallery_model.dart';
+import 'package:toonquirrel/services/api_comment.dart';
 import 'package:toonquirrel/services/api_gellary.dart';
+import 'package:http/http.dart' as http;
 
 class GalleryDetailScreen extends StatefulWidget {
   final int id;
@@ -27,19 +29,24 @@ class _GalleryDetailScreenState extends State<GalleryDetailScreen> {
   late Future<GalleryModel?> galleryDetailFuture;
   late SharedPreferences prefs;
   bool isLiked = false;
+  bool showComments = false;
+
+  TextEditingController commentController = TextEditingController();
+  List<Map<String, dynamic>> comments = [];
 
   @override
   void initState() {
     super.initState();
     initGalleryDetail();
     initPrefs();
+    loadComments();
   }
 
-  Future<void> initGalleryDetail() async {
-    final galleryData = await GalleryService.getGalleryById(context, widget.id);
-    final gallery = GalleryModel.fromJson(galleryData);
-    setState(() {
-      galleryDetailFuture = Future.value(gallery);
+  void initGalleryDetail() {
+    galleryDetailFuture =
+        GalleryService.getGalleryById(context, widget.id).then((galleryData) {
+      final gallery = GalleryModel.fromJson(galleryData);
+      return gallery;
     });
   }
 
@@ -57,6 +64,27 @@ class _GalleryDetailScreenState extends State<GalleryDetailScreen> {
     }
   }
 
+  void _showSnackbar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: const Duration(seconds: 1),
+      ),
+    );
+  }
+
+  Future<void> loadComments() async {
+    try {
+      final List<Map<String, dynamic>> commentList =
+          await CommentService.getCommentList(widget.id.toString());
+      setState(() {
+        comments = commentList;
+      });
+    } catch (e) {
+      print('Error loading comments: $e');
+    }
+  }
+
   onHeartTap() async {
     final likedGallery = prefs.getStringList('likedGallery');
     if (likedGallery != null) {
@@ -69,6 +97,19 @@ class _GalleryDetailScreenState extends State<GalleryDetailScreen> {
       setState(() {
         isLiked = !isLiked;
       });
+    }
+  }
+
+  Future<void> sendComment() async {
+    try {
+      final content = commentController.text;
+      await CommentService.createComment(
+          context, widget.id.toString(), content);
+      await loadComments();
+
+      commentController.clear();
+    } catch (e) {
+      print('Error sending comment: $e');
     }
   }
 
@@ -89,6 +130,14 @@ class _GalleryDetailScreenState extends State<GalleryDetailScreen> {
             ),
           ),
           IconButton(
+            onPressed: () {
+              setState(() {
+                showComments = !showComments;
+              });
+            },
+            icon: const Icon(Icons.comment),
+          ),
+          IconButton(
             onPressed: () {},
             icon: const Icon(Icons.share),
           ),
@@ -102,66 +151,91 @@ class _GalleryDetailScreenState extends State<GalleryDetailScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            AspectRatio(
-              aspectRatio: 16 / 9,
-              child: Image.network(
-                widget.photo,
-                fit: BoxFit.cover,
-              ),
+            Row(
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage(widget.photo),
+                ),
+                const SizedBox(width: 8),
+                Text(
+                  widget.name,
+                  style: const TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
             Padding(
               padding: const EdgeInsets.all(16),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    widget.name,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
                   FutureBuilder<GalleryModel?>(
                     future: galleryDetailFuture,
                     builder: (context, snapshot) {
                       if (snapshot.connectionState == ConnectionState.waiting) {
                         return const CircularProgressIndicator();
                       }
-
                       if (snapshot.hasError) {
                         return Text('Error: ${snapshot.error}');
                       }
-
                       final gallery = snapshot.data;
 
                       return Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const SizedBox(height: 16),
-                          const Text(
-                            'Contents:',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(height: 8),
+                          const SizedBox(height: 10),
                           if (gallery != null)
-                            ...gallery.contents.map(
-                              (content) {
-                                return Text(
-                                  content,
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                  ),
-                                );
-                              },
+                            SizedBox(
+                              height: 200,
+                              child: PageView.builder(
+                                itemCount: gallery.contents.length,
+                                itemBuilder: (context, index) {
+                                  final content = gallery.contents[index];
+                                  return AspectRatio(
+                                    aspectRatio: 16 / 9,
+                                    child: Image.network(
+                                      content,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  );
+                                },
+                              ),
                             ),
                         ],
                       );
                     },
                   ),
+                  const SizedBox(height: 30),
+                  if (showComments)
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        for (Map<String, dynamic> comment in comments)
+                          ListTile(
+                            title: Text(comment['member']['name'] as String),
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                  comment['member']['photo'] as String),
+                            ),
+                            subtitle: Text(comment['content'] as String),
+                          ),
+                        const SizedBox(height: 8),
+                        // 댓글 입력 폼
+                        TextFormField(
+                          controller: commentController,
+                          decoration: const InputDecoration(
+                            labelText: 'Add a comment...',
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        ElevatedButton(
+                          onPressed: sendComment,
+                          child: const Text('Send Comment'),
+                        ),
+                      ],
+                    ),
                 ],
               ),
             ),
